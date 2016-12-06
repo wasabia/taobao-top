b6da89caa66fe75b937848f34b805baa3967a7e8
zl <themorecolor@gmail.com>
2016-12-06 15:39:21 +0800

    ii

--- lib/taobao/rails/engine.rb
+++ lib/taobao/rails/engine.rb
@@ -3,8 +3,6 @@ module Taobao
     class Engine < ::Rails::Engine
 
       initializer "taobao.top" do |app|
-        ::Taobao::TOP.sandbox = !::Rails.env.production?
-
         if defined?(::OmniAuth::Strategies::Taobao)
           ::OmniAuth::Strategies::Taobao.option :client_options, ::Taobao::TOP.gateways
         end
